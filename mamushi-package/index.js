6a0d39961bcc8f751df88499340512dbed362fe8
AntoniaB21 <antonia.balluais@mail-ecv.fr>
2021-12-17 15:28:32 +0100

    export random element

--- mamushi-package/index.js
+++ mamushi-package/index.js
@@ -1,5 +1,6 @@
 const sortedArray = require('./sortedArray.js');
 const tri = require('./slug.js');
 const upperCase = require('./upperCase.js');
+const randomElement = require('./random-element.js');
 
-module.exports = { sortedArray, tri, upperCase};
+module.exports = { sortedArray, tri, upperCase, randomElement};
